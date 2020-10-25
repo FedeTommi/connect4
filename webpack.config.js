c1d7fadeca893db6aca0f7128b236bbc30668215
Federica Tomola <tomo97@hotmail.com>
2020-10-25 15:17:31 +0100

    Forward 404 to index.html
    This is required since we are using react router

--- webpack.config.js
+++ webpack.config.js
@@ -66,5 +66,10 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
+    historyApiFallback: {
+      rewrites: [
+        { from: /./, to: '/index.html' }
+      ]
+    }
   }
 }
