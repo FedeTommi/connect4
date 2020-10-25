9416c8d4339a387da6d2b18da0c13cfd071cbb87
Federica Tomola <tomo97@hotmail.com>
2020-10-25 15:24:58 +0100

    Add maple pizza logo on each page

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import withStyles from 'react-jss'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import LandingPage from './LandingPage'
+import MFLogo from "../svg/maple-pizza-logo.svg"
 
+const styles = {
+    maplePizza: {
+        width: 90,
+        position: 'absolute',
+        left: 10,
+        bottom: 10,
+    }
+}
 class App extends React.Component {
     render() {
+        const { classes } = this.props
+
         return <Router>
+            <MFLogo className={classes.maplePizza}/>
             <Switch>
                 <Route path="/">
                     <LandingPage />
@@ -15,4 +29,8 @@ class App extends React.Component {
     }
 }
 
-export default App
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(App)
